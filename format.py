21814424cfa0cce67de69d4455d55210774b3dd4
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-05-02 20:40:58 +0200

    Update format.py + format files

--- format.py
+++ format.py
@@ -2,14 +2,22 @@ import glob
 import os
 import subprocess
 import sys
+import threading
 
 def format_files(files):
+    blacklist = ["sound_data.tbl.inc.c"]
     for file in files:
-        subprocess.run(["clang-format", "-i", "-style=file", file])
+        if os.path.basename(file) not in blacklist:
+            print(f"Formatting {file}...")
+            subprocess.run(["clang-format", "-i", "-style=file", file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            print(f"Skipping {file}...")
 
 if len(sys.argv) > 1:
     print(f"formatting file(s) {sys.argv[1:]}")
-    format_files(sys.argv[1:])
+    format_thread = threading.Thread(target=format_files, args=(sys.argv[1:],))
+    format_thread.start()
+    format_thread.join()
     print("done.")
 else:
     print("formatting...")
@@ -17,5 +25,7 @@ else:
                       glob.glob("lib/src/*.[chin]", recursive=True) + \
                       glob.glob("data/**/*.[chin]", recursive=True) + \
                       glob.glob("enhancements/*.inc.[chin]", recursive=True)
-    format_files(files_to_format)
+    format_thread = threading.Thread(target=format_files, args=(files_to_format,))
+    format_thread.start()
+    format_thread.join()
     print("done.")
