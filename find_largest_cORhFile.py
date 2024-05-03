6e740fe49b937253660d4bd4f7bc672b896f52b4
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-05-03 03:36:36 +0200

    Update find_largest_cORhFile.py

--- find_largest_cORhFile.py
+++ find_largest_cORhFile.py
@@ -1,22 +1,29 @@
 import glob
 import os
 import heapq
+import logging
 
 def find_largest_files(files, n=100):
     sizes_files = [(os.path.getsize(file), file) for file in files]
     return heapq.nlargest(n, sizes_files)
 
+# Récupérer le nom du script sans l'extension
+script_name = os.path.splitext(os.path.basename(__file__))[0]
+
+# Configurer le logger pour écrire dans un fichier .txt
+logging.basicConfig(filename=f'{script_name}.txt', level=logging.INFO)
+
 directories = [
     "**/*.c", "**/*.h",
 ]
 
-print("Largest files:")
+logging.info("Largest files:")
 for directory in directories:
     files = glob.glob(directory, recursive=True)
     if files:
         largest_files = find_largest_files(files)
         for size, file in largest_files:
-            print(f"Directory: {directory}")
-            print(f"File: {file}")
-            print(f"Size: {size} bytes")
-            print()
+            logging.info(f"Directory: {directory}")
+            logging.info(f"File: {file}")
+            logging.info(f"Size: {size} bytes")
+            logging.info("")
